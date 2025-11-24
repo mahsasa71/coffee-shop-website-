ca3d99bbb8aacf72389b91082a368602f8aeab23
Mahsa <mahsasahraei71@gmail.com>
2025-11-24 20:15:35 +0330

    fifth2

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -8,19 +8,19 @@ export default function Info() {
             <div className="circle w-[165px] h-[165px] rounded-full">
                 <img src='https://avatars.githubusercontent.com/u/139154451?v=4' alt="myPicture" className="w-full h-full rounded-full object-cover" />
             </div>
-            <strong className="absolute top-[45%] right-[-87px] -rotate-[90deg] text-[15px] italic text-black">Frontend
+            <strong className="absolute top-[45%] left-[-87px] -rotate-[90deg] text-[15px] italic text-black">Frontend
                 Developer</strong>
             <a href="https://www.linkedin.com/in/mahsa-sahraei?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
-                <FaLinkedin className="group-hover:visible text-[88px] text-white absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 top-[-30px] right-[-53px]" />
+                <FaLinkedin className="group-hover:visible text-[88px] text-black absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 top-[-30px] right-[-53px]" />
             </a>
             <a href="https://www.instagram.com/mahsacodes?igsh=b3o4NXE4d25tYmli">
-                <FaInstagram className="group-hover:visible text-[88px] absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 text-white top-[20px] right-[-70px]" />
+                <FaInstagram className="group-hover:visible text-[88px] absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 text-black top-[20px] right-[-70px]" />
             </a>
             <a href="https://github.com/mahsasa71">
-                <FaGithub className="group-hover:visible text-[88px] absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 text-white top-[70px] right-[-70px]" />
+                <FaGithub className="group-hover:visible text-[88px] absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 text-black top-[70px] right-[-70px]" />
             </a>
             <a href="mailto:sahraee.mse@gmail.com">
-                <MdEmail className="group-hover:visible text-[88px] absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 text-white top-[110px] right-[-48px]" />
+                <MdEmail className="group-hover:visible text-[88px] absolute invisible transition-all duration-150 py-[5px] px-[30px] hover:opacity-900 text-black top-[110px] right-[-48px]" />
             </a>
         </div>
     )
